ff0fa622ec824e84b4ea9ba5fbf696bb6c9134ba
Apurva Kumar Gupta <akgupta24092002@gmail.com>
2024-05-11 23:37:52 +0530

    web socket

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -123,7 +123,7 @@ const Chat = ({ myUserId }) => {
     }
 
     function connectToWs() {
-        const ws = new WebSocket('wss://https://chitchat-ei78.onrender.com');
+        const ws = new WebSocket('wss://chitchat-ei78.onrender.com');
         setWs(ws);
         ws.addEventListener('message', handleIncomingMessage);
 
