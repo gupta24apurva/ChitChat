5488e3335ab4ba071cf269e7c765b35d4b87ec79
Apurva Kumar Gupta <akgupta24092002@gmail.com>
2024-05-24 18:55:47 +0530

    topbar

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -259,7 +259,7 @@ const Chat = ({ myUserId }) => {
             <div className='bg-blue-100 w-2/3 p-2 flex flex-col'>
                 {!!selectedUserId && (
                     <div className='fixed top-0 w-full z-10 bg-white border-b border-gray-200 py-2 pl-4 flex items-center'>
-                        <div>
+                        <div className='text-lg'>
                             <span>{onlinePeople[selectedUserId] || offlinePeople[selectedUserId]}</span>
                         </div>
                         <div className='text-sm ml-4'>
